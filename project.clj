9270516a65d1b9c97e10961b63cee9f07d7ca14c
Tom Marsh <ts.marsh@gmail.com>
2015-05-21 21:53:42 -0400

    Can write stl files

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [java3d/j3d-loader-core "1.1.0"]
-                 [java3d/j3d-loader-stl "1.1.0"]]
+                 [java3d/j3d-loader-stl "1.1.0"]
+                 [nio "1.0.3"]]
 
   :repositories [["lib" "file:lib"]])
