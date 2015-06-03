3dd6fe24fecac6cb0e8a67fa922940dc50eb5284
Tom Marsh <ts.marsh@gmail.com>
2015-06-03 07:35:58 -0400

    Renamed the vertices of the facet, they're ordered not named

--- src/main/clojure/stl_collector/model.clj
+++ src/main/clojure/stl_collector/model.clj
@@ -4,4 +4,4 @@
 (def Vertex [ (s/one Float "x") (s/one Float "y") (s/one Float "z")])
 
 (def Facet {:normal Vertex
-            :vertices [(s/one Vertex "xs") (s/one Vertex "ys") (s/one Vertex "zs")]})
+            :vertices [(s/one Vertex "1") (s/one Vertex "2") (s/one Vertex "3")]})
