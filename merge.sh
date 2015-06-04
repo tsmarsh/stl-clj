9ab173f8380b335c1ae7553b7935363ffe90c876
Tom Marsh <ts.marsh@gmail.com>
2015-06-04 02:48:28 -0400

    auto merge attempt 1

---
+++ merge.sh
@@ -0,0 +1,9 @@
+export GIT_COMMITTER_EMAIL=travis@tailoredshapes.com
+export GIT_COMMITTER_NAME=Travis CI
+
+echo $floop > key
+chmod 600 key
+ssh-add key
+git checkout master || exit
+git merge "$TRAVIS_COMMIT"
+git push 
