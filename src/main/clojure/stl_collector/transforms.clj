8df2e85e1a79cd5d143d08cf70f8b49811d6ad8c
Tom Marsh <ts.marsh@gmail.com>
2015-06-03 23:17:41 -0400

    Refactored and added schema validation to test

--- src/main/clojure/stl_collector/transforms.clj
+++ src/main/clojure/stl_collector/transforms.clj
@@ -2,10 +2,16 @@
   (:require [stl-collector.model :as m]
             [schema.core :as s]))
 
+(s/defn columise :- [(s/one [Float] "1s")
+                     (s/one [Float] "2s")
+                     (s/one [Float] "3s")]
+  [vertexes :- [m/Vertex]]
+  (apply map vector vertexes))
+
 (s/defn maxima :- m/Vertex
   [vertexes :- [m/Vertex]]
-  (map (partial apply max) (apply map vector vertexes)))
+  (map (partial apply max) (columise vertexes)))
 
 (s/defn minima :- m/Vertex
   [vertexes :- [m/Vertex]]
-  (map (partial apply min) (apply map vector vertexes)))
+  (map (partial apply min) (columise vertexes)))
--- src/test/clojure/stl_collector/transforms_test.clj
+++ src/test/clojure/stl_collector/transforms_test.clj
@@ -1,21 +1,28 @@
 (ns stl-collector.transforms-test
   (:require [stl-collector.transforms :as t]
+            [schema.test :as st]
             [clojure.test :refer :all]))
+
+(use-fixtures :once st/validate-schemas)
 
 (deftest maximum
   (testing "no maximum" 
-    (let [vertices [[0 0 0]]]
-      (is (= [0 0 0] (t/maxima vertices)))))
+    (let [vertices [[(float 0)(float 0)(float 0)]]]
+      (is (= [0.0 00. 0.0] (t/maxima vertices)))))
   (testing "a maximum"
-    (let [vertices [[1 8 3] [4 5 9] [7 2 6]]]
-      (is (= [7 8 9] (t/maxima vertices))))))
+    (let [vertices [[(float 1) (float 8) (float 3)]
+                    [(float 4) (float 5) (float 9)]
+                    [(float 7) (float 2) (float 6)]]]
+      (is (= [7.0 8.0 9.0] (t/maxima vertices))))))
 
 (deftest minimum
   (testing "no minimum"
-    (let [vertices [[0 0 0]]]
-      (is (= [0 0 0] (t/minima vertices))))
-    )
+    (let [vertices [[(float 0)(float 0)(float 0)]]]
+      (is (= [0.0 0.0 0.0] (t/minima vertices)))))
+  
   (testing "a minimum"
-    (let [vertices [[1 8 3] [4 5 9] [7 2 9]]]
-      (is (= [1 2 3] (t/minima vertices))))))
+    (let [vertices [[(float 1) (float 8) (float 3)]
+                    [(float 4) (float 5) (float 9)]
+                    [(float 7) (float 2) (float 6)]]]
+      (is (= [1.0 2.0 3.0] (t/minima vertices))))))
 
