236bc4f0fd9f19773ae8adfe381b9e6242744e8f
Tom Marsh <ts.marsh@gmail.com>
2015-06-04 03:05:31 -0400

    Adding echos for merge tracing

--- merge.sh
+++ merge.sh
@@ -4,6 +4,9 @@ export GIT_COMMITTER_NAME=Travis CI
 echo $floop > key
 chmod 600 key
 ssh-add key
+echo "Checking out master"
 git checkout master || exit
+echo "merging"
 git merge "$TRAVIS_COMMIT" || exit
+echo "Success!"
 git push
