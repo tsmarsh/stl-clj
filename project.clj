27d9ebafd5f9852268731f1dc803a94c798ec4ee
Tom Marsh <ts.marsh@gmail.com>
2015-06-04 01:57:48 -0400

    normals work normally

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"] 
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.mikera/core.matrix "0.35.0"]
                  [nio "1.0.3"]
                  [prismatic/schema "0.4.3"]]
 
