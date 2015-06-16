8bad706367305ba19231750e628f166d5075b0de
Tom Marsh <ts.marsh@gmail.com>
2015-06-16 17:36:56 -0400

    It fucking works!

--- src/main/clojure/stl_collector/core.clj
+++ src/main/clojure/stl_collector/core.clj
@@ -2,3 +2,12 @@
   (:require [stl-collector.reader :as r]
             [stl-collector.writer :as w]
             [stl-collector.transforms :as t]))
+
+
+(defn combine-files [output & filenames]
+  (let [stls (map r/read-stl filenames)
+        faces (map t/facify stls)
+        distributed-faces (t/distribute-x faces 10)
+        recombined-faces (t/combine distributed-faces)
+        normalized-faces (t/normalize recombined-faces)]
+    (w/write-stl normalized-faces output)))
