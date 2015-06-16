aa97ca9f00e29a734ba758141fba8a6d4bf0a8f7
Tom Marsh <ts.marsh@gmail.com>
2015-06-16 15:24:40 -0400

    Adding transforms to core

--- src/main/clojure/stl_collector/core.clj
+++ src/main/clojure/stl_collector/core.clj
@@ -1,3 +1,4 @@
 (ns stl-collector.core
   (:require [stl-collector.reader :as r]
-              [stl-collector.writer :as w]))
+            [stl-collector.writer :as w]
+            [stl-collector.transforms :as t]))
