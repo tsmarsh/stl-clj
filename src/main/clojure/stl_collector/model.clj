e4747ee7a94da9ac8667029fe6139993fae647e7
Tom Marsh <ts.marsh@gmail.com>
2015-06-16 15:20:37 -0400

    Added comments with example structures
    'cause I'm dumb and I was getting confused.
    
    I wonder if I should use generators

--- src/main/clojure/stl_collector/model.clj
+++ src/main/clojure/stl_collector/model.clj
@@ -1,9 +1,29 @@
 (ns stl-collector.model
   (:require [schema.core :as s]))
 
+;; [0.0 0.0 0.0] 
 (def Vertex [ (s/one Double "x") (s/one Double "y") (s/one Double "z")])
 
+;;[[0.0 0.0 0.0]
+;;  [0.0 0.0 0.0]
+;;  [0.0 0.0 0.0]]
 (def Face [(s/one Vertex "1") (s/one Vertex "2") (s/one Vertex "3")])
+
+;;{:normal [0.0 0.0 0.0]
+;; :vertices [[0.0 0.0 0.0]
+;;            [0.0 0.0 0.0]
+;;            [0.0 0.0 0.0]]}
 
 (def Facet {:normal Vertex
             :vertices Face})
+
+;;[ {:normal [0.0 0.0 0.0]
+;; :vertices [[0.0 0.0 0.0]
+;;            [0.0 0.0 0.0]
+;;            [0.0 0.0 0.0]]}
+;;{:normal [0.0 0.0 0.0]
+;; :vertices [[0.0 0.0 0.0]
+;;            [0.0 0.0 0.0]
+;;            [0.0 0.0 0.0]]}]
+(def STL [Facet])
+
