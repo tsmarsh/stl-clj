76075da8206630f2d3cdce9b8e3b3fc091fc22b0
Tom Marsh <tmarsh@hedgeserv.com>
2015-07-03 10:41:31 -0400

    Updated to 1.7
    It all worked. I know because I have test coverage, there was a compilation step and clojure is awesome.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.mikera/core.matrix "0.35.0"]
                  [nio "1.0.3"]
                  [prismatic/schema "0.4.3"]]
@@ -13,4 +13,4 @@
   :test-paths ["src/test/clojure"]
   :repositories [["lib" "file:lib"]]
   :plugins [[lein-cloverage "1.0.6"]
-            [lein-cljfmt "0.1.10"]])
+            [lein-cljfmt "0.1.12"]])
