e444a4dbc89b848d135900cdbf9678e98e295dde
Tom Marsh <ts.marsh@gmail.com>
2015-07-22 19:28:44 -0400

    Updating version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tsmarsh/stl-collector "0.2.1"
+(defproject tsmarsh/stl-collector "0.2.2"
   :description "Library for collecting multiple STL files into a single STL file"
   :url "http://tailoredshapes.com/"
   :license {:name "BSD"
